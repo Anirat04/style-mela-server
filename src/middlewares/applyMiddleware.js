e7fa9de3799dbdf9c1c791fbe08e5bc5fe550174
Tanbir Haque <tanbirhaque53@gmail.com>
2024-05-10 00:07:17 +0600

    Server corrs problem solved

--- src/middlewares/applyMiddleware.js
+++ src/middlewares/applyMiddleware.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const { 
-    LOCAL_CLIENT, 
-    // LOCAL_ADMIN_CLIENT, 
-    CLIENT 
-} = require('../config/default');
-
+const express = require("express");
+const cors = require("cors");
+const {
+  LOCAL_CLIENT,
+  LOCAL_ADMIN_CLIENT,
+//   CLIENT,
+} = require("../config/default");
 
 const applyMiddleware = (app) => {
-    app.use(cors({
-        origin: [
-            LOCAL_CLIENT,
-            // LOCAL_ADMIN_CLIENT,
-            CLIENT
-        ]
-    }));
+  app.use(
+    cors({
+      origin: [
+        LOCAL_CLIENT, 
+        LOCAL_ADMIN_CLIENT, 
+        // CLIENT
+    ],
+    })
+  );
 
-    app.use(express.json())
+  app.use(express.json());
+};
 
-}
-
-module.exports = applyMiddleware
+module.exports = applyMiddleware;
--- src/routes/products/index.js
+++ src/routes/products/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
-const { getALLProducts, addProduct } = require('../../api/products/controllers');
+const { getALLProducts, addProduct, getSingleProduct } = require('../../api/products/controllers');
 
 router.get("/all-products", getALLProducts);
+router.get("/single-product/:id", getSingleProduct);
 router.post("/add-product", addProduct);
 
 module.exports = router;
